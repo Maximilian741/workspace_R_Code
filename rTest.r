ecb03ffcfb1f0251a0247af0b11f0d58647dce51
Max Casteel <maximilian.casteel@umconnect.umt.edu>
2023-01-23 14:41:46 -0700

    Create rTest.r

---
+++ rTest.r
@@ -0,0 +1,26 @@
+#Vector 
+myVector <- c(1,2,3,4,5,6)
+myVector > 3
+myVector == 3
+
+
+#Concatenate
+myNumbers <- c(5,7,8)
+
+#Find the mean
+mean(myNumbers)
+
+
+#Randomly sample from a list of things
+sample(myNumbers, 2)
+
+#Create a vector of the numbers 2-11 w/c()
+myVector <- c(2:11)
+
+
+vecotrOfWords <- c("yellow", "green", "orange")
+
+
+#Sample from myVector 100 times
+sample(myVector, 100, replace = TRUE)
+
