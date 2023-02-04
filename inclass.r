128d14c9d64e2a255b5fcf8b4052ecd0f37f2015
Max Casteel <maximilian.casteel@umconnect.umt.edu>
2023-02-04 10:25:13 -0700

    In class r code

---
+++ inclass.r
@@ -0,0 +1,23 @@
+myVector <- c(3,12,8)
+sum(myVector)
+
+for(i in 1:3){
+  print(myVector[i] == 12)
+}
+
+set.seed(12)
+mySamples <- sample(myVector,100,replace = TRUE)
+print(mySamples)
+
+sum(mySamples)
+
+mySum <- c()
+for(i in 1:5){
+  mySum[i] <- c(mySamples[i])
+  print(mySamples[i])
+}
+
+
+sum(mySum)
+
+
